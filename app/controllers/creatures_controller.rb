4bc74d11cd95ba2a1f8f77b9120ea273334ce6c9
jroers <joe.roers@gmail.com>
2016-01-09 15:57:59 -0800

    Added index method

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,2 +1,10 @@
 class CreaturesController < ApplicationController
+	# Displays all creatures
+	def index
+		# Fetches all creatures from the database and save to the instance variable, @creatures
+		@creatures = Creature.all
+
+		# Renders the index view (gives it access to the instance variable)
+		render :index
+	end
 end

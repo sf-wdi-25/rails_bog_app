8c7598ce5c6356c86ee1e72560affbf40fb544ac
jroers <joe.roers@gmail.com>
2016-01-09 16:51:17 -0800

    Have new, create, and show working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -9,6 +9,35 @@ class CreaturesController < ApplicationController
 	end
 
 	def new
+		@creature = Creature.new
 		render :new
 	end
+
+	# create a new creature in the database
+  def create
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    # create a new creature from `creature_params`
+    creature = Creature.new(creature_params)
+
+    # if creature saves, redirect to route that displays all creatures
+    if creature.save
+      redirect_to creature_path(creature)
+      # redirect_to creatures_path is equivalent to:
+      # redirect_to "/creatures"
+    end
+  end
+
+  def show
+  	# Get the creature ID from the parameters
+  	creature_id = params[:id]
+
+  	# Searches the db for the instance of that id
+  	@creature = Creature.find_by_id(creature_id)
+
+  	# Makes @creature available to be displayed
+  	render :show
+  end
+
 end

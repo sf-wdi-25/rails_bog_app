cab2bc0eadfecb03515b4dd872dd095d52f22c59
kehontas <kehontas@gmail.com>
2016-01-09 20:22:27 -0800

    update after an ass kicking by typos

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -28,6 +28,7 @@ class CreaturesController < ApplicationController
 			#redirect_to creatures_path is equivalent to:
 			#redirect_to "/creatures#{creature.id}"
 		end
+	end
 
 	def show
 		#get the creature id from the url params
@@ -41,7 +42,37 @@ class CreaturesController < ApplicationController
 		render :show
 
 	end
-end
 
+	def edit
+		#get the creature id from teh url params
+		creature_id = params[:id]
+
+		#use 'creature_id' to find the creature in the database
+		#and save it to an instance variable
+		@creature = Creature.find_by_id(creature_id)
+
+		#render the edit view (it has access to the instance variable)
+		render :edit
+	end
+
+	#update the creature in the database
+	def update 
+		#get the creature id from the url params
+		creature_id = params[:id]
+
+		#use 'creature_id' to find the creature in the database
+		#and save it to an instance variable
+		creature = Creature.find_by_id(creature_id)
+
+		# whitelist params and save them to a variable
+		creature_params = params.require(:creature).permit(:name, :description)
+
+		creature.update_attributes(creature_params)
+
+		#redirect to show page for the updated creture
+		redirect_to creature_path(creature)
+		#redirect_to creature_path(creature) is equivalent to:
+		#redirect_to "/creatures/#{creature.id}"
+	end
 
 end

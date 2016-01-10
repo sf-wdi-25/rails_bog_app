8e197e0e611fb05d09bdef54aedd471ae62c5b4f
jroers <joe.roers@gmail.com>
2016-01-09 16:32:59 -0800

    Added 'new' route to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root "creatures#index"
   
-  get "/creatures", to: "creatures#index" , as: "creatures"
-
+  get "/creatures", to: "creatures#index"
+  get "/creatures/new", to: "creatures#new"
 end

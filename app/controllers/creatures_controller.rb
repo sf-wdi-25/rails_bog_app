a702f99f063ecab5285077222b9cb922f3916a0c
bachtsui <dbtsui2@gmail.com>
2016-01-10 12:42:59 -0800

    Finished Edit section, but there's a bug

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -29,4 +29,25 @@ class CreaturesController < ApplicationController
 		render :show
 	end
 
+	def edit
+		creature_id = params[:id]
+
+		@creature = Creature.find_by_id(creature_id)
+
+		render :edit
+	end
+
+	def update
+		creature_id = params[:id]
+
+		@creature = Creature.find_by_id(creature_id)
+		#needs @ here?
+
+		creature_params = params.require(:creature).permit(:name, :description)
+
+		@creature.update_attributes(creature_params)
+
+		redirect_to creatures_path(creature)
+	end
+
 end

c3c663a259ec12d1bf39dcea4eaa54a0a10dff86
bachtsui <dbtsui2@gmail.com>
2016-01-10 15:08:04 -0800

    Edit works now, missed a BACKSLASH! for a route

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -40,12 +40,12 @@ class CreaturesController < ApplicationController
 	def update
 		creature_id = params[:id]
 
-		@creature = Creature.find_by_id(creature_id)
+		creature = Creature.find_by_id(creature_id)
 		#needs @ here?
 
 		creature_params = params.require(:creature).permit(:name, :description)
 
-		@creature.update_attributes(creature_params)
+		creature.update_attributes(creature_params)
 
 		redirect_to creatures_path(creature)
 	end

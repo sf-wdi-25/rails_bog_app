913cd28207f276ae940b10d7ef91ea77098fbf97
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-10 20:55:29 -0800

    Edit is working

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "/creatures/new", to: "creatures#new", as: "new_creature"
   post "/creatures" => "creatures#create"
   get "/creatures/:id" => "creatures#show", as: "creature"
-  
+  get "/creatures/:id/edit" => "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"
 end

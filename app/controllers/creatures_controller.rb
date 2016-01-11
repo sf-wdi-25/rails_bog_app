def1d2d604ffa1f199301b6439edca090d6ab8f2
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-10 20:50:37 -0800

    new, create, and show working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -14,7 +14,7 @@ class CreaturesController < ApplicationController
 		creature_params = params.require(:creature).permit(:name, :description)
 		creature = Creature.new(creature_params)
     if creature.save
-			redirect_to creatures_path
+			redirect_to creatures_path(creature)
 		end
 	end
 

a9afa86686b5148a9052f859701770361d82da70
Breon Knight <Breonknight7@gmail.com>
2016-01-10 22:48:41 -0800

    show is working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -12,7 +12,21 @@ class CreaturesController < ApplicationController
 
 	#shows new creature form
 	def new
+		@creature = Creature.new
 		render :new
+	end
+
+	def create
+		#white lists params and saves them to a varaible
+		creature_params = params.require(:creature).permit(:name, :description)
+
+		#creating a new creature from the parameter of creature
+		creature = Creature.new(creature_params)
+
+		#if it saves then it refreshes the page and displays all the creatures
+		if creature.save
+			redirect_to creatures_path
+		end
 	end
 
 end

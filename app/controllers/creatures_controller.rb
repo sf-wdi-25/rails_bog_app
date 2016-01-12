e100ea709c977a8162e186d49234f24cf895625d
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-11 18:46:20 -0800

    refactored stuffs?

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -16,7 +16,7 @@ class CreaturesController < ApplicationController
     creature = Creature.new(creature_params)
 
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
   end
 

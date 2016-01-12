5577c811dc6fa8b5d24886c005489ccefcf317e2
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-11 18:42:47 -0800

    commit message

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -20,4 +20,12 @@ class CreaturesController < ApplicationController
     end
   end
 
+  def show
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   post "/creatures", to: "creatures#create"
 
+  get "/creatures/:id", to: "creatures#show", as: "creature"
+
 end

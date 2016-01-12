c1ebea37cff0beae76da646b366526abcdb47c95
anonym0us3 <nathan.shayevich@gmail.com>
2016-01-11 18:30:39 -0800

    set up the creatures 'create' action

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -9,4 +9,14 @@ class CreaturesController < ApplicationController
     render :new
   end
 
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get "/creatures/new", to: "creatures#new"
 
+  post "/creatures", to: "creatures#create"
+
 end
